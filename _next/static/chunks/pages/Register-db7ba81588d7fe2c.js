85dc05c54509529c84a8606ea13bbf13d4891187
Kiri <tsarenkokir@gmail.com>
2024-12-28 17:08:45 +0300

    Delete _next/static directory

--- _next/static/chunks/pages/Register-db7ba81588d7fe2c.js
+++
@@ -1,1 +0,0 @@
-(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[37],{7664:function(n,_,u){(window.__NEXT_P=window.__NEXT_P||[]).push(["/Register",function(){return u(542)}])}},function(n){n.O(0,[664,409,774,888,179],(function(){return _=7664,n(n.s=_);var _}));var _=n.O();_N_E=_}]);
